411416f3097a9c4d5c92e7959b99b3017baee238
Temitope Akande <topsiology11@gmail.com>
2023-03-15 07:26:39 +0100

    debug

--- components/Access.js
+++ components/Access.js
@@ -5,7 +5,7 @@ function Access() {
           <h1 className="text-[40px] leading-[40px] sm:leading-[92px] sm:text-[60px] lg:text-[82px] font-black text-[#050F3A] text-center mb-4 sm:mb-9 ">Access Customers Data With No Code</h1>
           <p className="text-sm font-bold text-[#4D4D4D] opcaity-70 font-Montserrat sm:text-lg mb-4 sm:mb-8">A well-designed data infrastructure can help you implement the necessary security measures, such as encryption, access controls, and monitoring</p>
           <div className="-mt-4">
-            <button className="bg-[#293CE2] text-white rounded-md px-2 py-1 sm:py-1.5 sm:px-2 lg:px-5 shadow mx-1 sm:mx-1 text-xs mb-2 sm:text-lg hover:brightness-75 font-bold">Get Started</button>
+            <button className="bg-[#293CE2] text-white rounded-md px-3 py-3 mt-2 sm:py-1.5 sm:px-2 lg:px-5 shadow mx-1 sm:mx-1 text-xs mb-2 sm:text-lg hover:brightness-75 font-bold">Get Started</button>
             <button className=" rounded-md px-2 py-1 sm:py-1.5 sm:px-1.5 lg:px-5 shadow mx-1 sm:mx-2 text-xs sm:text-lg text-[#293CE1] hover:bg-gray-300 font-bold">Book a Demo</button>
           </div>
           <div className="sm:mt-24 mb-2 sm:mb-2">
